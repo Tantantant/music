798e212a3c9f08ff4044667945fcc66d979c0021
Tantantant <499221986@qq.com>
2021-01-04 18:31:50 +0800

    feat:拦截器

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@ module.exports = {
     devServer: {
         proxy: {
             "/api": {
-                target: "http://192.168.20.43:9091",
+                target: "http://192.168.20.57:9091/music",
                 changeOrigin: true, // 允许跨域
-                // pathRewrite: { // 重写路径
-                //   "^/api": "",
-                // },
+                pathRewrite: { // 重写路径
+                  "^/api": "",
+                },
             },
         },
     },
