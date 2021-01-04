a450af43f5c1b0a11359f99ab81821015b6f2296
Tantantant <499221986@qq.com>
2021-01-04 13:49:37 +0800

    feat:代理服务器

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
         proxy: {
             "/api": {
-                target: "https://y.qq.com",
+                target: "http://192.168.20.43:9091",
                 changeOrigin: true, // 允许跨域
                 // pathRewrite: { // 重写路径
                 //   "^/api": "",
